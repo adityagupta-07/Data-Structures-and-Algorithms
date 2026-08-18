d2676fb72c8d99508f94044d8c5310ef4899a7bf
Aditya Kumar Gupta <aditya.gupta@ultratendency.com>
2026-08-18 16:15:05 +0545

    Doubly linked list prepend

--- Doubly Linked List/Doubly Linked List.py
+++ Doubly Linked List/Doubly Linked List.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, value):
-        self.value = value
         self.next = None
+        self.value = value
         self.previous = None
 
 
@@ -21,3 +21,16 @@ class DoublyLinkedList:
             self.tail = new_node
         self._length += 1
         return self
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.previous = new_node
+            self.head = new_node
+        self._length += 1
+        return self
+
+
