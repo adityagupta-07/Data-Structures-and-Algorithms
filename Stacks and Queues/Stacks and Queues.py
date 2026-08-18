a7c85231286d53008778ad8759dde1c6fdc57ae5
Aditya Kumar Gupta <aditya.gupta@ultratendency.com>
2026-08-18 17:59:38 +0545

    Stack pop, peek, clear

--- Stacks and Queues/Stacks and Queues.py
+++ Stacks and Queues/Stacks and Queues.py
@@ -21,3 +21,32 @@ class Stack:
         self._top = new_element
         self._size += 1
         return self
+
+    def pop(self):
+        if not self._size:
+            raise Exception("stack is empty")
+        former_top = self._top
+        self._top = former_top.next
+        former_top.next = None
+        self._size -= 1
+        return former_top.value
+
+    def peek(self):
+        if self._top:
+            return self._top.value
+        return None
+
+    def clear(self):
+        self._top = None
+        self._size = 0
+        return self
+
+
+a = Stack()
+
+a.push("google")
+a.push(9)
+a.pop()
+
+print(a.peek())
+print(len(a))
