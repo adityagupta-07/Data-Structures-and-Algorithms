c4d3caebdb64fab13c2be20f1808f24b08877668
Aditya Kumar Gupta <aditya.gupta@ultratendency.com>
2026-08-25 20:50:13 +0545

    Append multiple items into SLL

--- Singly Linked List/Singly Linked List.py
+++ Singly Linked List/Singly Linked List.py
@@ -30,15 +30,16 @@ class SinglyLinkedList:
     def __iter__(self):
         return LinkedListIterator(self.head)
 
-    def append(self, value):
-        new_node = Node(value)
-        if self._length == 0:
-            self.head = self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
-        self._length += 1
-        return self
+    def append(self, *values): # accepts arbitary number of arguments and packs them into a tuple
+            for value in values:
+                new_node = Node(value)
+                if self._length == 0:
+                    self.head = self.tail = new_node
+                else:
+                    self.tail.next = new_node
+                    self.tail = new_node
+                self._length += 1
+            return self
 
     def pop_left(self):
         if not self._length:
@@ -117,21 +118,22 @@ class SinglyLinkedList:
     
 
 a = SinglyLinkedList()
+b = SinglyLinkedList()
 
 
-a.append(21)
+
 a.append(5)
 a.append(12)
 a.append(7)
-a.append(10)
-a.append(9)
-a.append(8)
-a.append(14)
-a.append(6)
-a.append(19)
-a.append(4)
+a.append(21, 10, 6, 19, 4)
+b.append(9)
+b.append(8)
+b.append(14, 6, 19, 88, 71, 31, 5, 0)
+b.append(4)
 
-print(f"Number of nodes: {len(a)}")
-print(f"Head: {a.head.value}, Tail: {a.tail.value}")
+# print(f"Number of nodes: {len(a)}")
+# print(f"Number of nodes: {len(b)}")
+# print(f"Head: {a.head.value}, Tail: {a.tail.value}")
 
 a.traverse()
+b.traverse()
